c134e24e0a2e2e04e5cc804fc9bddfdd98af0782
hurr11cane1 <chosh.choshov.88@gmail.com>
2025-01-12 19:32:31 +0300

    The Tower Class

---
+++ Source/ToonTanks/Tower.h
@@ -0,0 +1,34 @@
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "BasePawn.h"
+#include "Tower.generated.h"
+
+
+
+UCLASS()
+class TOONTANKS_API ATower : public ABasePawn
+{
+	GENERATED_BODY()
+	
+
+public:
+
+	virtual void Tick(float DeltaTime) override;
+
+
+protected:
+
+	virtual void BeginPlay() override;
+
+
+
+private:
+
+	class ATank* Tank;
+
+	UPROPERTY(EditDefaultsOnly, Category = "Combat")
+	float FireRange = 300.f;
+
+};
